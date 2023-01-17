ad597befde6f0fd14fee7b36d31872be5994fb92
DigitalSpice <lmcurry93@hotmail.com>
2023-01-16 22:15:51 -0800

    change

--- src/Card.js
+++ src/Card.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 const Card = ({title, id, image_id, date_display, artist_display}) => {
     return (
-        <div className='bg-lightest-blue ma2 flex flex-column flex-row-ns items-center'>
-            <div className='pr3-ns mb4 mb0-ns w-100 w-40-ns'>
-                <img alt='art' src={`https://www.artic.edu/iiif/2/${image_id}/full/200,/0/default.jpg`} />
+        <div className='bg-lightest-blue ma2 flex flex-column flex-row-ns items-center w-50 center grow'>
+            <div className='pr3-ns mb4 mb0-ns w-40-ns flex items-center'>
+                <img alt='art' src={`https://www.artic.edu/iiif/2/${image_id}/full/400,/0/default.jpg`} />
             </div>
-            <div className='w-100 w-60-ns pl3-ns'>
-                <h2 className='lh-title'>{title}</h2>
+            <div className='w-100 w-60-ns pr4-ns'>
+                <h2 className='lh-title mb0'>{title}</h2>
                 <p className='lh-copy'>
-                    {date_display}<br/>
-                    {artist_display}<br/>
-                    Gallery ID: {id}
+                    <em>Finished: {date_display}</em><br/>
+                    <b>{artist_display}</b><br/>
+                    Art Institute of Chicago Gallery ID: {id}
                 </p>
             </div>
         </div>
