09913abd23521d4969190b45704dd7f06e2ae56a
DigitalSpice <lmcurry93@hotmail.com>
2023-01-16 17:30:41 -0800

    layout change

--- src/Card.js
+++ src/Card.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 const Card = ({title, id, image_id, date_display, artist_display}) => {
     return (
-        <div className='db bg-lightest-blue br2 ma3 grow shadow-3 pv4'>
-            <img alt='art' className='' src={`https://www.artic.edu/iiif/2/${image_id}/full/200,/0/default.jpg`} />
-            <div className='pa2'>
-                <h2>{title}</h2>
-                <h3>{date_display}</h3>
-                <p>{artist_display}</p>
-                <p>Gallery ID: {id}</p>
+        <div className='bg-lightest-blue ma2 flex flex-column flex-row-ns items-center'>
+            <div className='pr3-ns mb4 mb0-ns w-100 w-40-ns'>
+                <img alt='art' src={`https://www.artic.edu/iiif/2/${image_id}/full/200,/0/default.jpg`} />
+            </div>
+            <div className='w-100 w-60-ns pl3-ns'>
+                <h2 className='lh-title'>{title}</h2>
+                <p className='lh-copy'>
+                    {date_display}<br/>
+                    {artist_display}<br/>
+                    Gallery ID: {id}
+                </p>
             </div>
         </div>
     );
